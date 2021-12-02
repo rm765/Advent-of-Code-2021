89794db6945625ad7577734485b07c9ac0a66056
rm765 <93552195+rm765@users.noreply.github.com>
2021-12-02 19:40:56 +0100

    Add files via upload

---
+++ AoC2021Day1/AoC2021Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿List<int> lst = new List<int>();
+foreach (string line in File.ReadAllLines(@"input.txt"))
+    {
+    lst.Add(int.Parse(line));
+}
+
+//Part 1
+int increased = 0;
+for (int i = 1; i < lst.Count; i++)
+{
+    if (lst[i] > lst[i-1])
+    {
+        increased++;
+    }
+}
+Console.WriteLine(increased);
+
+//Part 2
+increased = 0;
+int sumNew = 0;
+int sumOld = lst[2] + lst[1] + lst[0];
+for (int i = 3; i < lst.Count; i++)
+{
+
+    sumNew = lst[i] + lst[i - 1] + lst[i - 2];
+
+    if (sumNew > sumOld)
+    {
+        increased++;
+    }
+    sumOld = sumNew;
+
+}
+Console.WriteLine(increased);
---
+++ AoC2021Day2/AoC2021Day2/Program.cs
@@ -0,0 +1,57 @@
+﻿var lines = File.ReadAllLines(@"input.txt").ToList();
+
+int depth = 0;
+int horizontal = 0;
+int final;
+
+//Part 1
+foreach (var line in lines)
+{
+    var splitStr = line.Split(' ', 2);
+    string direction = splitStr[0];
+    int distance = int.Parse(splitStr[1]);
+    if (direction == "up")
+    {
+        depth = depth + distance;
+    }
+    if (direction == "down")
+    {
+        depth = depth - distance;
+    }
+    if (direction == "forward")
+    {
+        horizontal = horizontal + distance;
+    }
+
+
+}
+final = Math.Abs(horizontal * depth);
+Console.WriteLine("Part 1: " + final);
+
+//Part 2
+int aim = 0;
+depth = 0;
+horizontal = 0;
+foreach (var line in lines)
+{
+    var splitStr = line.Split(' ', 2);
+    string direction = splitStr[0];
+    int distance = int.Parse(splitStr[1]);
+    if (direction == "up")
+    {
+        aim = aim + distance;
+    }
+    if (direction == "down")
+    {
+        aim = aim - distance;
+    }
+    if (direction == "forward")
+    {
+        horizontal = horizontal + distance;
+        depth = depth + (aim * distance);
+    }
+
+
+}
+final = Math.Abs(horizontal * depth);
+Console.WriteLine("Part 2: " + final);
